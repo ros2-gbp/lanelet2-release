eda9281a7611936eb010681e45228eea04392049
Fabian Immel <fabian.immel@kit.edu>
2026-06-18 15:13:59 +0000

    Trimmed the branch to only the lanelet2_traffic_rules sub directory

--- lanelet2_python/typings/lanelet2/__init__.pyi
+++
@@ -1,4 +0,0 @@
-from lanelet2 import core, geometry, io, routing, traffic_rules, matching, projection
-
-__all__ = ["core", "geometry", "io", "routing",
-           "traffic_rules", "matching", "projection"]
